09a0a4893ae64336e924b98c0ff07a9a8f7c9169
Riya Shrestha <rshrest2@stevens.edu>
2021-01-26 18:34:54 -0600

    "Added a colors file in config folder."

---
+++ App/Config/colors.js
@@ -0,0 +1,10 @@
+const { exp } = require("react-native/Libraries/Animated/src/Easing");
+
+export default {
+    primary: '#E63946',
+    secondary: '#F1FAEE',
+    light: '#A8DADC',
+    medium: '#457B9D',
+    dark: '#1D3557',
+
+}
