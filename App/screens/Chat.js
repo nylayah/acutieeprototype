268cf2529bcc1e722b5bf80557f39ed2347f52a3
Nylayah Jones <njones3@stevens.edu>
2021-02-15 14:12:30 -0500

    added chat format and samples, update acutiee contact card

--- App/screens/Chat.js
+++ App/screens/Chat.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { ImageBackground, View, Image, Alert, SafeAreaView, StyleSheet, Text, TextInput, Button, TouchableOpacity } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { ImageBackground,Platform,KeyboardAvoidingView, View, Image, Alert, SafeAreaView, StyleSheet, Text, TextInput, Button, TouchableOpacity } from 'react-native';
 import { colors, styles } from '../Config/Styles';
 import { Ionicons, AntDesign } from '@expo/vector-icons';
+import { ScrollView } from 'react-native-gesture-handler';
+
+
 
 
 function ChatScreen({ navigation }) {
-    return (
-        <SafeAreaView style={{ flexDirection: "column", backgroundColor: colors.medium, alignItems: "center", flex: 1, }}>
+    
+    return(
+      
+      <SafeAreaView style={{ flexDirection: "column", backgroundColor: colors.medium, alignItems: "center", flex: 1, }}>
+           
             <View style={styles.chatContainer} style={{ flex: 1.25, flexDirection: 'row', width: "100%", alignItems: "center" }} contentContainerStyle={{ width: `${100 * 3}%` }}>
                 <View style={{ backgroundColor: colors.medium, flex: 1, alignItems: "center", }} />
                 <TouchableOpacity style={{ backgroundColor: colors.medium, flex: 1, alignItems: "center", }} onPress={() => navigation.navigate('Edit Avatar')}>
@@ -15,14 +21,32 @@ function ChatScreen({ navigation }) {
                 <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={{ backgroundColor: colors.medium, flex: 1, alignItems: "center", paddingTop: 0 }}>
                     <Ionicons name="settings" size={35} style={styles.iconStyleS} />
                 </TouchableOpacity>
-
-                <View />
+                <View />    
             </View>
-            <View style={{ backgroundColor: colors.light, alignItems: "flex-end", flex: 15, width: "100%", justifyContent: "flex-end", padding: 10, flexDirection: "row" }}>
+            <Text style={{color:"white", fontSize:12,}}>Acutiee</Text>
+            
+           
+            <View style={{ backgroundColor: colors.light, alignItems: "flex-end", flex: 15, width: "100%", justifyContent: "flex-end", padding: 10, }}>
+                <ScrollView contentContainerStyle={{backgroundColor: colors.light, width: "100%", padding: 10,justifyContent:"flex-end"}}>
+                   <View style={styles.chatmessageuser}>
+                    <Text style={styles.chattextuser}>heyy!</Text>
+                   </View>
+                   <View style={styles.chatmessageacutiee}>
+                    <Text style={styles.chattextacutiee}>hey welcome to acutiee!</Text>
+                   </View>
+                   <View style={styles.chatmessageacutiee}>
+                    <Text style={styles.chattextacutiee}>you can text me anything you want to know or need help with regarding ypur health</Text>
+                   </View>
+                   <View style={styles.chatmessageacutiee}>
+                    <Text style={styles.chattextacutiee}>I'll set up a plan to help you efficently reach your goals, just let me know what they are.</Text>
+                   </View>
+                </ScrollView>
+                <View style={{flexDirection:"row", }}>
                 <TextInput style={styles.sendMessage} placeholder="Send Message..." />
                 <TouchableOpacity>
-                    <AntDesign name="upcircle" size={35} style={styles.iconStyleR} />
+                    <AntDesign name="upcircle" size={40} style={styles.iconStyleR} />
                 </TouchableOpacity>
+                </View>
             </View>
 
             <View style={{ backgroundColor: colors.medium, bottom: 0, flexDirection: "row", flex: 1.25, alignItems: "center", }} contentContainerStyle={{ width: `${100 * 3}%` }}>
@@ -36,7 +60,9 @@ function ChatScreen({ navigation }) {
                     <Ionicons name="chatbubble-ellipses" size={35} style={styles.iconStyleS} />
                 </TouchableOpacity>
             </View>
+        
         </SafeAreaView>
+        
     );
 
 
