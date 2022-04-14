54076dd0ad4a1c09007ad63eeaa35930b30d9a02
Dean <deanju1102@gmail.com>
2022-04-14 13:39:10 -0400

    code review

--- App/screens/HomeScreen.js
+++ App/screens/HomeScreen.js
@@ -8,11 +8,13 @@ function HomeScreen({ navigation }) {
     return (
         <SafeAreaView style={{ flexDirection: "column", backgroundColor: colors.medium, alignItems: "center", flex: 1, }}>
             <View style={{ flex: 15 }}>
-                <View style={styles.chatContainer} style={{ flex: 1.25, flexDirection: 'row', width: "100%", alignItems: "center" }} contentContainerStyle={{ width: `${100 * 3}%` }}>
+                <View style={{ flex: 1.25, flexDirection: 'row', width: "100%", alignItems: "center" }} contentContainerStyle={{ width: `${100 * 3}%` }}>
                     <View style={{ backgroundColor: colors.medium, flex: 1, alignItems: "center", }} />
+
                     <TouchableOpacity style={{ backgroundColor: colors.medium, flex: 1, alignItems: "center", }} onPress={() => navigation.navigate('Edit Avatar')}>
                         <Image source={require("../assets/User.jpg")} style={{ width: 50, height: 50, borderRadius: 25 }} />
                     </TouchableOpacity>
+                    
                     <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={{ backgroundColor: colors.medium, flex: 1, alignItems: "center", paddingTop: 0 }}>
                         <Ionicons name="settings" size={35} style={styles.iconStyleS} />
                     </TouchableOpacity>
@@ -44,6 +46,7 @@ function HomeScreen({ navigation }) {
                                 ]
                                 )}>Walk for 10 mins</Text>
                         </TouchableOpacity>
+
                         <TouchableOpacity style={styles.homeTab}>
                             <Text style={styles.homeTabText} onPress={() =>
                                 Alert.alert("To Do", "Make salad for dinner", [
@@ -53,6 +56,7 @@ function HomeScreen({ navigation }) {
                                 ]
                                 )}>Make salad for dinner</Text>
                         </TouchableOpacity>
+
                         <TouchableOpacity style={styles.homeTab}>
                             <Text style={styles.homeTabText} onPress={() =>
                                 Alert.alert("To Do", "Drink 64 oz of water", [
@@ -62,6 +66,7 @@ function HomeScreen({ navigation }) {
                                 ]
                                 )}>Drink 64 oz of water</Text>
                         </TouchableOpacity>
+
                         <TouchableOpacity style={styles.homeTab}>
                             <Text style={styles.homeTabText} onPress={() =>
                                 Alert.alert("To Do", "10 squats per hour", [
@@ -71,6 +76,7 @@ function HomeScreen({ navigation }) {
                                 ]
                                 )}>10 squats per hour</Text>
                         </TouchableOpacity>
+
                         <TouchableOpacity style={styles.homeTab} >
 
                             <Text style={styles.homeTabText} onPress={() =>
@@ -81,6 +87,7 @@ function HomeScreen({ navigation }) {
                                 ]
                                 )}>Check fiber intake</Text>
                         </TouchableOpacity>
+
                         <TouchableOpacity style={styles.homeTab} onPress={() =>
                             Alert.alert("To Do", "Log lunch", [
                                 { text: "Done", onPress: () => console.log("Done") },
@@ -99,10 +106,12 @@ function HomeScreen({ navigation }) {
                             <MaterialCommunityIcons name="silverware-fork-knife" size={30} style={styles.iconStyleS} />
                             <Text style={styles.homeTabText}>Current Eating habits</Text>
                         </TouchableOpacity>
+
                         <TouchableOpacity style={styles.homeTabC}>
                             <FontAwesome5 name="running" size={30} style={styles.iconStyleS} />
                             <Text style={styles.homeTabText}>Activities</Text>
                         </TouchableOpacity>
+
                         <TouchableOpacity style={styles.homeTabC}>
                             <Foundation name="target" size={30} style={styles.iconStyleS} />
                             <Text style={styles.homeTabText}>Goals</Text>
@@ -129,10 +138,12 @@ function HomeScreen({ navigation }) {
                         <TouchableOpacity style={styles.homeTab}>
                             <Text style={styles.homeTabText}>Read</Text>
                         </TouchableOpacity>
+
                         <TouchableOpacity style={styles.homeTab} >
 
                             <Text style={styles.homeTabText}>Watch</Text>
                         </TouchableOpacity>
+
                         <TouchableOpacity style={styles.homeTab}>
                             <Text style={styles.homeTabText}>Listen</Text>
                         </TouchableOpacity>
@@ -143,9 +154,11 @@ function HomeScreen({ navigation }) {
                 <TouchableOpacity style={{ flex: 1, alignItems: "center", }} onPress={() => navigation.navigate('Home')}>
                     <Ionicons name="home" size={35} style={styles.iconStyleS} />
                 </TouchableOpacity>
+
                 <View style={{ flex: 1, alignItems: "center" }}>
                     <Image source={require("../assets/FitAll.png")} style={{ width: 50, height: 50, marginTop: '10%' }} />
                 </View>
+                
                 <TouchableOpacity style={{ flex: 1, alignItems: "center", justifyContent: "flex-end" }} onPress={() => navigation.navigate('Chat')}>
                     <Ionicons name="chatbubble-ellipses" size={35} style={styles.iconStyleS} />
                 </TouchableOpacity>
